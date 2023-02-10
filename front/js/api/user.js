46e128262c3e011d4178df9380ec56bc81c316aa
Badr Al Achkar <alachkarbadr@gmail.com>
2023-02-10 18:17:21 +0100

    delete : removed some comments

--- front/js/api/user.js
+++ front/js/api/user.js
@@ -21,21 +21,9 @@ export const signup = (body) => async (dispatch) => {
   try {
     const res = await api.post(rootPath+"/api/signin", body);
     console.log(res);
-    // const res = await axios.post('/users/register',body,config)
-    // dispatch({
-    //     type:REGISTER_SUCCESS,
-    //     payload:res.data
-    // })
-    // dispatch(setAlert('Signed Up succesfully','success'))
-    // dispatch(loadUser())
+
   } catch (err) {
     console.log(err);
-    // if (errors) {
-    //     errors.forEach(error => dispatch(setAlert(error.message, 'danger')))
-    // }
-    // dispatch({
-    //     type: REGISTER_FAIL
-    // })
   }
 };
 
@@ -44,19 +32,7 @@ export const login = (body) => async (dispatch) => {
   try {
     const res = await api.post(rootPath+"/api/login", body);
     console.log(res);
-    // dispatch({
-    //     type: LOGIN_SUCCESS,
-    //     payload: res.data
-    // })
-    // dispatch(loadUser())
-    // dispatch(setAlert('Logged in successfully',"success"))
   } catch (err) {
     console.log(err);
-    // if (errors) {
-    //     errors.forEach(error => dispatch(setAlert(error.message, 'danger')))
-    // }
-    // dispatch({
-    //         type: LOGIN_FAIL
-    // })
   }
 };
