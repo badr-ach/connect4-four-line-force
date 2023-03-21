89a80739a57634ffd7a36a06b26a07026ae9670e
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2023-03-21 18:12:28 +0100

    [DEV] profile page handling friends invitations

---
+++ front/js/components/profileComponent/profileComponent.js
@@ -0,0 +1,51 @@
+const user = {
+    name: "John Smith",
+    email: "john.smith@example.com",
+};
+
+const friends = ["Jane Doe", "Mark Johnson", "Emily Lee"];
+
+const invitations = ["David Kim", "Amy Chen", "Tom Brown"];
+
+document.getElementById("name").innerText = user.name;
+document.getElementById("email").innerText = user.email;
+
+const friendList = document.getElementById("friends");
+friends.forEach(friend => {
+    const li = document.createElement("li");
+    li.innerText = friend;
+    friendList.appendChild(li);
+});
+
+const invitationList = document.getElementById("invitations");
+invitations.forEach(invitation => {
+    //const li = document.createElement("li");
+    //li.innerText = invitation;
+    //li.classList.add("pending");
+    //invitationList.appendChild(li);
+    const li = document.createElement("li");
+    li.innerText = invitation;
+    li.classList.add("pending");
+
+    const acceptButton = document.createElement("button");
+    acceptButton.innerText = "Accepter";
+    acceptButton.addEventListener("click", () => {
+        // Remove invitation from the list of pending invitations
+        invitationList.removeChild(li);
+
+        // Add friend to the list of friends
+        const newFriend = document.createElement("li");
+        newFriend.innerText = invitation;
+        friendList.appendChild(newFriend);
+    });
+    li.appendChild(acceptButton);
+
+    const rejectButton = document.createElement("button");
+    rejectButton.innerText = "Refuser";
+    rejectButton.addEventListener("click", () => {
+        invitationList.removeChild(li);
+    });
+    li.appendChild(rejectButton);
+
+    invitationList.appendChild(li);
+});
