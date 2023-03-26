44c98ae5c125325763b63a0283559511a222e30c
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2023-03-26 12:38:04 +0200

    update profile

--- front/js/components/profileComponent/profileComponent.js
+++ front/js/components/profileComponent/profileComponent.js
@@ -1,30 +1,20 @@
 
-import { Animator } from "../../scripts/animator.js";
-import { events } from "../../events/events.js";
-import {logout} from "../../api/user.js";
-import {SideBar} from "../test/sideBarComponent.js";
-
-
-
-this.user = {
+const user = {
     name: "John Smith",
     email: "john.smith@example.com",
 };
-this.friends = ["Jane Doe", "Mark Johnson", "Emily Lee"];
+const friends = ["Jane Doe", "Mark Johnson", "Emily Lee"];
 
-this.invitations = ["David Kim", "Amy Chen", "Tom Brown"];
+const invitations = ["David Kim", "Amy Chen", "Tom Brown"];
 
+document.getElementById("name").innerText = user.name;
+document.getElementById("email").innerText = user.email;
 
-
-
-    document.getElementById("name").innerText = user.name;
-    document.getElementById("email").innerText = user.email;
-
-    const friendList = document.getElementById("friends");
-    friends.forEach(friend => {
-    const li = document.createElement("li");
-    li.innerText = friend;
-    friendList.appendChild(li);
+const friendList = document.getElementById("friends");
+friends.forEach(friend => {
+const li = document.createElement("li");
+li.innerText = friend;
+friendList.appendChild(li);
 });
 
 const invitationList = document.getElementById("invitations");
