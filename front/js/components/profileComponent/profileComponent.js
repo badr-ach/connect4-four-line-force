25c1e1abed585e89b78ec8ab0e46539439fba28d
SAISSIOMAR <omar.saissi@etu.univ-cotedazur.fr>
2023-03-27 15:48:49 +0200

    profile

--- front/js/components/profileComponent/profileComponent.js
+++ front/js/components/profileComponent/profileComponent.js
@@ -1,54 +1,81 @@
+import {Animator} from "../../scripts/animator.js";
 
-const user = {
-    name: "John Smith",
-    email: "john.smith@example.com",
-};
-const friends = ["Jane Doe", "Mark Johnson", "Emily Lee"];
+export class Register extends HTMLElement{
+    constructor(app){
+        super();
+        this.attachShadow({mode: "open"});
+        this._app = app;
+        this._animator = new Animator();
+        this.user = {
+            name: "John Smith",
+            email: "john.smith@gmail.com",
+        };
+        this.friends = ["Jane Doe", "Mark Johnson", "Emily Lee"];
+        this.invitations = ["David Kim", "Amy Chen", "Tom Brown"];
 
-const invitations = ["David Kim", "Amy Chen", "Tom Brown"];
+        this.writeUserData();
 
-document.getElementById("name").innerText = user.name;
-document.getElementById("email").innerText = user.email;
 
-const friendList = document.getElementById("friends");
-friends.forEach(friend => {
-const li = document.createElement("li");
-li.innerText = friend;
-friendList.appendChild(li);
-});
+    }
 
-const invitationList = document.getElementById("invitations");
-invitations.forEach(invitation => {
-    //const li = document.createElement("li");
-    //li.innerText = invitation;
-    //li.classList.add("pending");
-    //invitationList.appendChild(li);
-    const li = document.createElement("li");
-    li.innerText = invitation;
-    li.classList.add("pending");
 
-    const acceptButton = document.createElement("button");
-    acceptButton.innerText = "Accepter";
-    acceptButton.addEventListener("click", () => {
-        // Remove invitation from the list of pending invitations
-        invitationList.removeChild(li);
 
-        // Add friend to the list of friends
-        const newFriend = document.createElement("li");
-        newFriend.innerText = invitation;
-        friendList.appendChild(newFriend);
-    });
-    li.appendChild(acceptButton);
+    async connectedCallback(){
+        this.shadowRoot.innerHTML = await fetch("./js/components/profileComponent/profileComponent.html")
+            .then((r) => r.text())
+            .then((html) => html);
 
-    const rejectButton = document.createElement("button");
-    rejectButton.innerText = "Refuser";
-    rejectButton.addEventListener("click", () => {
-        invitationList.removeChild(li);
-    });
-    li.appendChild(rejectButton);
+        this._attachEventListeners();
+    }
 
-    invitationList.appendChild(li);
-});
+    writeUserData() {
+        this.shadowRoot.getElementById("name").innerText = this.user.name;
+        this.shadowRoot.getElementById("email").innerText = this.user.email;
+
+
+        const friendList = this.shadowRoot.getElementById("friends");
+        this.friends.forEach(friend => {
+            const li = this.shadowRoot.ownerDocument.createElement("li");
+            li.innerText = friend;
+            friendList.appendChild(li);
+        });
+
+        const invitationList = this.shadowRoot.ownerDocument.getElementById("invitations");
+        this.invitations.forEach(invitation => {
+
+            const li = this.shadowRoot.ownerDocument.createElement("li");
+            li.innerText = invitation;
+            li.classList.add("pending");
+
+            const acceptButton = this.shadowRoot.createElement("button");
+            acceptButton.classList.add("accept-button");
+            acceptButton.innerText = "Accepter";
+            acceptButton.addEventListener("click", () => {
+                invitationList.removeChild(li);
+
+                const newFriend = this.shadowRoot.createElement("li");
+                newFriend.innerText = invitation;
+                friendList.appendChild(newFriend);
+            });
+            li.appendChild(acceptButton);
+
+            const rejectButton = this.shadowRoot.createElement("button");
+            rejectButton.classList.add("accept-button");
+
+            rejectButton.innerText = "Refuser";
+            rejectButton.addEventListener("click", () => {
+                invitationList.removeChild(li);
+            });
+            li.appendChild(rejectButton);
+
+            invitationList.appendChild(li);
+        });
+    }
+
+
+}
+
+customElements.define("profile-component", Register);
 
 
 
