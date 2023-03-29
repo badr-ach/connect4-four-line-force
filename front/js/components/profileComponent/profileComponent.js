f5f8ecc23fa99884b730c79aa084c5040e5ddb16
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2023-03-29 18:32:14 +0200

    profile done with history and stats

--- front/js/components/profileComponent/profileComponent.js
+++ front/js/components/profileComponent/profileComponent.js
@@ -13,6 +13,7 @@ export class ProfileComponent extends HTMLElement{
         this.historyGames = [];
         const token = localStorage.getItem("token");
         this.api.use({Authorization: "Bearer " + token});
+        this.username = "";
 
     }
 
@@ -32,6 +33,7 @@ export class ProfileComponent extends HTMLElement{
 
         const res = await this.api.post(this.rootPath + "/api/loadUser");
         console.log("user is ", res.user.username);
+        this.username = res.user.username;
         this.shadowRoot.getElementById("userName").innerHTML = res.user.username;
         this.shadowRoot.getElementById("email").innerHTML = res.user.mail;
     }
@@ -40,7 +42,35 @@ export class ProfileComponent extends HTMLElement{
 
         const res = await this.api.post(this.rootPath + "/api/history");
         console.log("history is ", res);
-        this.shadowRoot.getElementById("w").innerHTML = res.history.winner;
+        this.shadowRoot.getElementById("games").innerHTML = res.history.length;
+
+        console.log("wins is ", res.history.filter(x => x.winner === this.username));
+        this.shadowRoot.getElementById("online-wins").innerHTML = res.history.filter(x => x.winner === this.username).length;
+        console.log("against",res.history[0]);
+        console.log("winner ",res.history[0].winner)
+        this._fillUpHistory(res.history);
+    }
+
+    _fillUpHistory(history){
+        /*let history = [
+            { against: "AI", result: "Win" },
+            { against: "Human", result: "Loss" },
+            { against: "Computer", result: "Tie" }
+        ];*/
+        const historyTable = this.shadowRoot.getElementById("history-table");
+        history.forEach(game => {
+            console.log("against",game.playerTwo);
+            console.log("winner ",game.winner)
+            const tr = document.createElement("tr");
+            const againstTd = document.createElement("td");
+            againstTd.textContent = game.playerTwo;
+            const resultTd = document.createElement("td");
+            resultTd.textContent = game.winner === game.playerOne ? "Win" : "Loss";
+            tr.appendChild(againstTd);
+            tr.appendChild(resultTd);
+            historyTable.appendChild(tr);
+
+        });
     }
 
 }
